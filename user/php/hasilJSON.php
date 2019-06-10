625d7cecb2b196bfb23c6bba6dd673d84fbe23f5
iqbalreact <iqbalmobiledev@gmail.com>
2019-06-10 15:12:11 +0700

    fix fitur histori

---
+++ user/php/hasilJSON.php
@@ -0,0 +1,26 @@
+<?php
+ 
+include('db_connect.php');
+
+
+ $query = "SELECT * FROM `log_diagnosis` INNER JOIN `penyakit` ON log_diagnosis.kd_penyakit = penyakit.kd_penyakit WHERE id_user=".$id_user;
+ $result = mysqli_query($link,$query) or die(mysqli_error());	
+
+//  print_r($result);
+
+ $arr = array();
+	while ($row = mysqli_fetch_assoc($result)) {
+	    $temp = array(
+	    "id_log"=> $row["id_log"],
+	    "id_user"=> $row["id_user"],
+	    "penyakit"=> $row["nm_penyakit"],
+	    "nilaicf"=> $row["nilaicf"],
+	    "date" => $row["date_diagnosis"]);
+	    array_push($arr, $temp);
+	}
+	 
+	$data = json_encode($arr);
+	echo $data;
+	 
+	/*echo "{\"data\":" . $data . "}";*/
+?>
