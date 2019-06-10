a12f5963dd71f693c8225f0be65c11dfc08f7515
iqbalreact <iqbalmobiledev@gmail.com>
2019-06-10 10:04:10 +0700

    histori API by id

--- user/php/historiJSON.php
+++ user/php/historiJSON.php
@@ -1,15 +1,21 @@
 <?php
  
 include('db_connect.php');
- 
- $query = "SELECT * FROM `log_diagnosis` WHERE id_user=";
- $result = mysqli_query($link,$query) or die(mysqli_error());
+
+ $id_user = $_GET['id'];
+ $query = "SELECT * FROM `log_diagnosis` INNER JOIN `penyakit` ON log_diagnosis.kd_penyakit = penyakit.kd_penyakit WHERE id_user=".$id_user;
+ $result = mysqli_query($link,$query) or die(mysqli_error());	
+
+//  print_r($result);
 
  $arr = array();
 	while ($row = mysqli_fetch_assoc($result)) {
 	    $temp = array(
-	    "Penyakit"=> $row["nm_penyakit"],
-	    "Deskripsi" => $row["deskripsi"]);
+	    "id_log"=> $row["id_log"],
+	    "id_user"=> $row["id_user"],
+	    "penyakit"=> $row["nm_penyakit"],
+	    "nilaicf"=> $row["nilaicf"],
+	    "date" => $row["date_diagnosis"]);
 	    array_push($arr, $temp);
 	}
 	 
